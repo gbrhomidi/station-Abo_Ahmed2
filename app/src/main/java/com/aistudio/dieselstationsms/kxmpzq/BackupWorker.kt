cbb85d5f42aea2fb50f4f4336f4ae44517ac76bb
gbrhomidi <gbrhomidi@gmail.com>
2026-07-21 05:20:14 +0300

    Update BackupWorker.kt

--- app/src/main/java/com/aistudio/dieselstationsms/kxmpzq/BackupWorker.kt
+++ app/src/main/java/com/aistudio/dieselstationsms/kxmpzq/BackupWorker.kt
@@ -13,7 +13,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-/**
+/*
  * ═══════════════════════════════════════════════════════════════
  * BackupWorker - عامل النسخ الاحتياطي التلقائي
  * ═══════════════════════════════════════════════════════════════
