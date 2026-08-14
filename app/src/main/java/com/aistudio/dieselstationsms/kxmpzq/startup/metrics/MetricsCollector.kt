ee48685af5b1a326f47ecbe5a13dd59aeb644dc2
اسمك <@gmail.com>
2026-08-15 02:38:42 +0300

    Clear all files before full replacement

--- app/src/main/java/com/aistudio/dieselstationsms/kxmpzq/startup/metrics/MetricsCollector.kt
+++
@@ -1,7 +0,0 @@
-package com.aistudio.dieselstationsms.kxmpzq.startup.metrics
-
-interface MetricsCollector {
-    fun record(key: String, value: Any)
-    fun getMetrics(): Map<String, Any>
-    fun clear()
-}
