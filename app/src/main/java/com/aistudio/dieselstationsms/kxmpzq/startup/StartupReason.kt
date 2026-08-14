9c56da8a5ee5d866e28fc6dc720338b3dbd3d952
اسمك <@gmail.com>
2026-08-14 23:08:23 +0300

    Clear all files before full replacement

--- app/src/main/java/com/aistudio/dieselstationsms/kxmpzq/startup/StartupReason.kt
+++
@@ -1,6 +0,0 @@
-package com.aistudio.dieselstationsms.kxmpzq.startup
-
-enum class StartupReason {
-    BOOT, APP_UPDATED, TIME_CHANGED, TIMEZONE_CHANGED,
-    ALARM, MANUAL, CRASH_RECOVERY, SCHEDULED, USER_UNLOCKED
-}
